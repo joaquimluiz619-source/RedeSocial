75829868442e11def19f2113636b47fa4c6844be
joaquimluiz619-source <joaquimluiz619@gmail.com>
2026-09-09 11:05:13 -0300

    ok

---
+++ JS script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DomContentLoaded', ()=>{
+   const LikeBtn = document.querySelector(''. like-btn'');
+   const postMedia = document.querySelector(". post-media"); 
+   if (!likeBtn) return;
+
+   cont likesCountSpan = Likebtn.querySelector(".likes-count");
+   const BookmarkBtn = document.querySelector(".bookn-btn")
+
+   let isLiked = false; 
+   let baseLikes = 0;
+
+   
+})
